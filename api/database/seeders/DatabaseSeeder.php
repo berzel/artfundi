008ad8707ad24dde80b9c835761fd36ea32fadf5
Berzel Best <berzel.best@gmail.com>
2025-06-16 16:58:09 +0200

    enhancement: seed 20 clients in DB

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Client;
 use App\Models\Permission;
 use App\Models\Role;
 use App\Models\User;
@@ -25,5 +26,7 @@ class DatabaseSeeder extends Seeder
 
         $user->addRole($admin);
         $admin->givePermission($createClient);
+
+        Client::factory()->count(20)->create();
     }
 }
